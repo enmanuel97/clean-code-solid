a8eaf2a95cf802dd18704b0c7ba313821b0b2776
Jesus Enmanuel De La Cruz <edelacruz9713@gmail.com>
2022-06-28 07:10:46 -0400

    Fix

--- src/clean-code/03-functions.ts
+++ src/clean-code/03-functions.ts
@@ -1,36 +1,56 @@
 (() => {
 	// funcion para obtener informacion de una pelicula por ID
-	function getAllMovies(movieId: string) {
+	function getAllMovieById(movieId: string) {
 		console.log({movieId})
 	}
 
 	// function para obtener informacion de los actores de una pelicula - Actors o Cast // id = movieId getMovieCast
-	function getAllMovieActors(id: string) {
+	function getAllMovieCastById(id: string) {
 		console.log({id})
 	}
 
 	// funcion para obtener el bio del actor por el id
-	function getUsuario(ActorId: string) {
+	function getActorBioById(ActorId: string) {
 		console.log({ActorId})
 	}
 
 	// Crear una pelicula
-	function movie(title: string, description: string, rating: number, cast: string[]) {
-		console.log({title, description, rating, cast})
+	interface Movie {
+		title: string;
+		description: string;
+		rating: number;
+		cast: string[];
+	}
+
+	function createMovie(movie: Movie) {
+		console.log(movie)
 	}
 	
-	// Crea un numero actor
 	function createActorIfActorNoExists(fullName: string, birthDate: Date): boolean {
 
 		// tarea asincrona para verifica nombre
 		// ..
 		// ..
 
-		if(fullName === 'Fernando') {
-			return false;
-		}
+		if(checkIfActorExists(fullName) == true) return false;
 
 		console.log('Crear actor');
 		return true;
 	}
+
+	function checkIfActorExists(fullName: string) {
+		return fullName === 'Fernando' ? true : false;
+	}
+
+	const getPayAmount = ({ isDead = false, isSeparated = true, isRetired = false }) => {
+        let result: number;
+
+        if ( isDead ) return 1500;
+		
+        if ( isSeparated ) return 2500;
+
+		result = isRetired ? 3000 : 2000;
+        
+        return result;
+    }
 });
